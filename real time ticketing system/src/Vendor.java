453657dfa863701eba8992e5e2f3424446fedea6
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-11-17 14:51:31 +0530

    update

--- real time ticketing system/src/Vendor.java
+++ real time ticketing system/src/Vendor.java
@@ -1,7 +1,7 @@
 public class Vendor implements Runnable {
-    private TicketPool ticketPool_for_vendor;
-    private int release_Rate;
-    private int total_Tickets;
+    private final TicketPool ticketPool_for_vendor;
+    private final int release_Rate; //release rate
+    private final int total_Tickets;//total ticket vendor can release at atime
 
 
     public Vendor(TicketPool ticketPool, int release_Rate, int total_Tickets) {
@@ -12,13 +12,16 @@ public class Vendor implements Runnable {
     @Override
     public void run() {
         try {
+
             for (int i = 0; i < total_Tickets; i++) {
+                //add vendors ticket to ticketpool
                 ticketPool_for_vendor.Add_Ticket(i);
+                //pause the thread for specific time
                 Thread.sleep(release_Rate);
             }
         }catch (Exception e){
-            System.out.println("Thread interrupted due to |"+e.getMessage());
-            Thread.currentThread().interrupt();
+            System.out.println("Thread interrupted due to |"+e.getMessage());//if their any exception show to user
+            Thread.currentThread().interrupt();//make thread Interrupt if there is any error
         }
     }
 
